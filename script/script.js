133b513e8d59fa2ddaedb562b8a5b18b45f67138
Pedro Povoa <phenriquepovo00@gmail.com>
2025-08-27 20:35:00 -0300

    criandobotao

--- script/script.js
+++ script/script.js
@@ -2,3 +2,9 @@ console.log(document.body);
 
 const ListaItens= document.getElementsByTagName("li");
 console.log(ListaItens);
+
+const botao= document.getElementById('btn');
+botao.addEventListener('click', function() {
+    alert("Você cliclou no botão!")
+});
+
