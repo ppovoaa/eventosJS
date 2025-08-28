414c9851a2268fed9954340aee1574095ce379cd
Pedro Povoa <phenriquepovo00@gmail.com>
2025-08-27 21:13:25 -0300

    botaofunciona

--- script/script.js
+++ script/script.js
@@ -3,9 +3,9 @@ console.log(document.body);
 const ListaItens= document.getElementsByTagName("li");
 console.log(ListaItens);
 
-const botao = document.getElementById('btn');
-botao.addEventListener('click', function() {
-    alert("Você cliclou no botão!")
-});
+// const botao = document.getElementById('btn');
+// botao.addEventListener('click', function() {
+//     alert("Você cliclou no botão!")
+// });
 
 
